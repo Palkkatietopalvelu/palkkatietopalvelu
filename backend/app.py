8c3d1affa48be1d82c253c799a96c0a8ab08e755
alannesanni <sanni.alanne@helsinki.fi>
2024-01-17 12:42:25 +0200

    Integration of frontend and backend

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+# backend/app.py
+
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
-def index():
-    return "Heipparallaa!"
+@app.route('/api/data')
+def get_data():
+    data = [{'id': 1, 'name': 'Example 1'}, {'id': 2, 'name': 'Example 2'}]
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run()
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,25 @@
-const App = () => (
-  <div>
-    <p>palkkatietopalvelu</p>
-  </div>
-)
+import { useEffect, useState } from 'react'
+
+const App = () => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:5000/api/data')
+      const result = await response.json()
+      console.log(result)
+      setData(result)
+    }
+
+    fetchData()
+  }, [])
+
+  return (
+    <div>
+      <h1>Backend Data:</h1>
+        {data.map(info => info.name)}
+    </div>
+  )
+}
 
 export default App
