e9156fa57e4e47f88d5c03c835fadcec96bfc330
alannesanni <sanni.alanne@helsinki.fi>
2024-01-24 12:07:44 +0200

    Fix adding client, remove material_id, lint fixes

--- backend/src/controllers/clients.py
+++ backend/src/controllers/clients.py
@@ -1,13 +1,11 @@
 from flask import Flask, request, jsonify
 from app import app
-from datetime import datetime
-from utilities.client_methods import add_new_client, get_all_clients_from_db, get_client_info_from_db
-    
+from utilities.client_methods import add_new_client, get_all_clients_from_db
+
 @app.route("/api/all_clients")
 def get_all_clients():
     try:
         all_clients = get_all_clients_from_db()
-        print(all_clients)
         return jsonify(all_clients)
     except Exception as error:
         return str(error), 400
@@ -17,17 +15,7 @@ def get_all_clients():
 def add_client():
     try:
         client_data = request.json
-        deadline_str = client_data.get("deadline")
-        deadline = datetime.strptime(deadline_str, "%Y-%m-%d").date()
-        add_new_client(
-            company=client_data.get("company"),
-            email=client_data.get("email"),
-            phonenumber=client_data.get("phonenumber"),
-            bi_code=client_data.get("bi_code"),
-            deadline=deadline,
-            payperiod=client_data.get("payperiod"),
-        )
+        add_new_client(client_data)
         return "Client added successfully", 201
     except Exception as error:
         return str(error), 400
-    
--- backend/src/utilities/client_methods.py
+++ backend/src/utilities/client_methods.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from sqlalchemy.sql import text
 from db import db
 
@@ -7,8 +8,17 @@ def get_all_clients_from_db():
     all_companies = [row[0] for row in result.fetchall()]
     return all_companies
 
-def add_new_client(company: str, email: str, phonenumber: str, bi_code: str, deadline, payperiod: str, material_id: int):
-    sql = text("INSERT INTO customers (company, email, phonenumber, bi_code, deadline, payperiod, material_id) VALUES (:company, :email, :phonenumber, :bi_code, :deadline, :payperiod, :material_id)")
-    db.session.execute(sql, {"company": company, "email": email, "phonenumber": phonenumber, "bi_code": bi_code, "deadline": deadline, "payperiod": payperiod, "material_id": material_id })
+def add_new_client(client_data):
+    deadline_str = client_data.get("deadline")
+    deadline = datetime.strptime(deadline_str, "%Y-%m-%d").date()
+    sql = text("""INSERT INTO customers (company, email, phonenumber, bi_code, deadline, payperiod)
+               VALUES (:company, :email, :phonenumber, :bi_code, :deadline, :payperiod)""")
+
+    db.session.execute(sql, {   "company": client_data.get("company"),
+                                "email": client_data.get("email"),
+                                "phonenumber": client_data.get("phonenumber"),
+                                "bi_code": client_data.get("bi_code"),
+                                "deadline": deadline,
+                                "payperiod": client_data.get("payperiod")})
     db.session.commit()
     
