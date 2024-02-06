87c2e57dcaebb2e8baf6bd55ef95491625558a88
liisa <liisa-m@outlook.com>
2024-02-06 19:56:36 +0200

    pylint fixes

--- backend/src/utilities/require_login.py
+++ backend/src/utilities/require_login.py
@@ -1,6 +1,6 @@
 import os
-from flask import request, jsonify
 from functools import wraps
+from flask import request, jsonify
 import jwt
 
 def require_login(f):
@@ -11,13 +11,13 @@ def require_login(f):
             token = request.headers['Authorization'].split()[1]
         if not token:
             return jsonify({'message' : 'Token puuttuu'}), 401
-        
+
         try:
-            user_data = jwt.decode(token, os.environ.get('SECRET_KEY'), algorithms=['HS256'])
-        except:
+            user_data = jwt.decode(token, os.environ.get('SECRET_KEY'), algorithms=['HS256']) # pylint: disable=unused-variable
+        except: # pylint: disable=bare-except
             return jsonify({
                 'message' : 'Virheellinen token'
             }), 401
         return  f(*args, **kwargs)
-  
+
     return decorated
