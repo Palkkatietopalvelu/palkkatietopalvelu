55c9a1611c3de66e7eae47cda3b1da4c6325dad6
Elias Toukolehto <elias.toukolehto@helsinki.fi>
2024-02-07 12:52:11 +0200

    fix import issues

--- backend/src/controllers/users.py
+++ backend/src/controllers/users.py
@@ -1,7 +1,7 @@
 from flask import request, jsonify
 from werkzeug.security import generate_password_hash
-from ..models.user import User, db
-from ..app import app
+from models.user import User, db
+from app import app
 
 @app.route('/api/users', methods=['GET'])
 def get_users():
--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -1,4 +1,4 @@
-from ..db import db
+from db import db
 
 class User(db.Model):
     __tablename__ = 'users'
