8784292ef3570818fee479f845007f91eecb7390
hakkajoe <joelhakkarainen95@gmail.com>
2024-02-08 16:52:01 +0200

    fixed small mistake in database init

--- backend/src/initialize_db.py
+++ backend/src/initialize_db.py
@@ -21,6 +21,7 @@ def create_tables():
     db.session.execute(text("""
         CREATE TABLE clients (
             id SERIAL PRIMARY KEY,
+            user_id INTEGER REFERENCES users,
             company TEXT,
             email TEXT,
             phonenumber TEXT,
@@ -40,7 +41,7 @@ def drop_tables():
     """))
 
     db.session.execute(text("""
-        DROP TABLE IF EXISTS clients;
+        DROP TABLE IF EXISTS clients CASCADE;
     """))
 
     db.session.commit()
