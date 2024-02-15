9d66c4b23fa4ec1c506fe022f2461421809df2b8
liisa <liisa-m@outlook.com>
2024-02-15 16:08:40 +0200

    add togglable

---
+++ frontend/src/components/Togglable.jsx
@@ -0,0 +1,41 @@
+import { useState, useImperativeHandle, forwardRef } from 'react'
+import { Button } from 'react-bootstrap'
+import PropTypes from 'prop-types'
+
+const Togglable = forwardRef((props, ref) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+    //props.resetFields()
+  }
+
+  useImperativeHandle(ref, () => {
+    return {
+      toggleVisibility
+    }
+  })
+
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        <Button onClick={toggleVisibility}>{props.buttonLabel}</Button>
+      </div>
+      <div style={showWhenVisible}>
+        {props.children}
+        <Button onClick={toggleVisibility}>peruuta</Button>
+      </div>
+    </div>
+  )
+})
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired
+}
+
+Togglable.displayName = 'Togglable'
+
+export default Togglable
