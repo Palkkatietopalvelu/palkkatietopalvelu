2206b4b46ab57b0980cac5f09bfb1f94274d2d06
Joacim-S <45919018+Joacim-S@users.noreply.github.com>
2024-02-16 00:28:44 +0200

    Implement fetching data for auto-reminders and improve scheduling

--- backend/src/app.py
+++ backend/src/app.py
@@ -8,6 +8,12 @@ from config import DATABASE_URL, TEST_DATABASE_URL, ENV
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['MAIL_SERVER']='sandbox.smtp.mailtrap.io'
+app.config['MAIL_PORT'] = 2525
+app.config['MAIL_USERNAME'] = getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = getenv('MAIL_PASSWORD')
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 
 if ENV == "development":
     app.config["SQLALCHEMY_DATABASE_URI"] = TEST_DATABASE_URL
@@ -24,7 +30,7 @@ from controllers import users, clients, login, mail
 from mail_scheduler import start_scheduler
 # pylint: enable=unused-import,wrong-import-position
 
-start_scheduler()
+start_scheduler('9')
 
 if __name__ == '__main__':
     #db.init_app(app)
--- backend/src/mail_scheduler.py
+++ backend/src/mail_scheduler.py
@@ -1,32 +1,69 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
-from utilities import client_methods
+from utilities import client_methods as clients
 from flask_mail import Mail, Message
 from app import app
 from os import getenv
+from datetime import timedelta, date
 
-app.config['MAIL_SERVER']='sandbox.smtp.mailtrap.io'
-app.config['MAIL_PORT'] = 2525
-app.config['MAIL_USERNAME'] = getenv('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = getenv('MAIL_PASSWORD')
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
 mail = Mail(app)
 
-def reminder():
+def send_reminders():
     with app.app_context():
+        deadlines, emails = get_reminder_data()
+        for deadline, email in zip(deadlines, emails):
+            print(deadline)
+        print(email)
+    """
         recipient = "placeholdermaili@gmail.com"
         msg = Message('Muistutus',
                     sender = app.config['MAIL_USERNAME'],
                     recipients = [recipient])
         msg.body = '''Hei! Tämä on automaattinen muistutus
         palkka-ainestojen toimituksen lähestyvästä eräpäivästä.'''
-        mail.send(msg)
-
-def start_scheduler():
+        #mail.send(msg)
+        print(msg)
+    """
+def start_scheduler(
+    hour,
+    days: list = 'mon-fri',
+    minute = '0',
+    second = '0'):
+    '''
+    Args:
+        hour: hour for sending e-mails
+        days (list): Abbrevations of weekdays
+        minute: minute for sending e-mails
+        second: second for senging e-mails
+    '''
     sched = BackgroundScheduler(daemon=True)
     trigger = CronTrigger(
-        year="2025", month="*", day="*", hour="*", minute="*", second="*",
+        day_of_week = str(days),
+        hour = str(hour),
+        minute = str(minute),
+        second = str(second)
     )
-    sched.add_job(reminder,trigger = trigger)
+    sched.add_job(send_reminders,trigger = trigger)
     sched.start()
+    return True
+
+def get_reminder_data():
+    deadlines, client_ids = get_deadline_data()
+    emails = get_emails(client_ids)
+    return deadlines, emails
+
+def get_deadline_data():
+    deadlines_with_ids = clients.get_next_deadlines()
+    deadlines = []
+    client_ids = []
+    for deadline in deadlines_with_ids:
+        if deadline.next_deadline - date.today() <= timedelta(days=100):
+            deadlines.append(deadline.next_deadline)
+            client_ids.append(deadline.client_id)
+    return deadlines, client_ids
+
+def get_emails(client_ids):
+    emails = []
+    for c_id in client_ids:
+        emails.append(clients.get_email(c_id))
+    return emails
