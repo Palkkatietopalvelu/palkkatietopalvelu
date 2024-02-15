f42f1b323c1f3d22d2ab6cc8475e29a5e90df502
liisa <liisa-m@outlook.com>
2024-02-15 16:07:10 +0200

    update credentials validation; add password change functionality

--- backend/src/controllers/users.py
+++ backend/src/controllers/users.py
@@ -1,7 +1,8 @@
 from flask import request, jsonify
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from models.user import User, db
 from app import app
+from utilities.require_login import require_login
 
 @app.route('/api/users', methods=['GET'])
 def get_users():
@@ -16,22 +17,57 @@ def create_user(testing="no", username="", password="", role=""):
         password = data['password']
         role = data['role']
 
-    if len(password) < 3 or len(username) < 3:
-        return jsonify(
-            {"error": "Käyttäjätunnus ja salasana täytyy olla ainakin 3 merkkiä pitkiä"}
-        ), 400
+    try:
+        validate_credentials(username, password)
+        hashed_password = generate_password_hash(password)
+        new_user = User(username=username, password=hashed_password, role=role)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify(new_user.serialize()), 201
+    except ValueError as error:
+        return str(error), 400
 
-    if len(password) > 15 or len(username) > 15:
-        return jsonify(
-            {"error": "Käyttäjätunnus ja salasana ei saa olla yli 15 merkkiä pitkiä"}
-        ), 400
+@app.route('/api/users/<int:user_id>', methods=['POST'])
+@require_login
+def change_password(user_id):
+    try:
+        data = request.get_json()
+        user = User.query.filter_by(id=user_id).first()
+        old_password = data["oldPassword"]
+        new_password = data["newPassword"]
+        confirm_password = data["confirmPassword"]
+        validate_password(old_password, new_password, confirm_password, user)
+        user.password = generate_password_hash(new_password)
+        db.session.add(user)
+        db.session.commit()
+        return "Salasana vaihdettu", 200
+    except ValueError as error:
+        return str(error), 400
 
+def validate_credentials(username, password):
     existing_user = User.query.filter_by(username=username).first()
     if existing_user:
-        return jsonify({"error": "Käyttäjätunnus on jo olemassa"}), 400
+        raise ValueError ("Käyttäjätunnus on jo olemassa")
 
-    hashed_password = generate_password_hash(password)
-    new_user = User(username=username, password=hashed_password, role=role)
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify(new_user.serialize()), 201
+    if len(username) < 3 or len(password) < 3:
+        raise ValueError ("Käyttäjätunnus ja salasana täytyy olla ainakin 3 merkkiä pitkiä")
+
+    if len(username) > 15:
+        raise ValueError ("Käyttäjätunnus ja salasana ei saa olla yli 15 merkkiä pitkiä")
+
+    return True
+
+def validate_password(old_password, password, confirm_password, user):
+    if not check_password_hash(user.password, old_password):
+        raise ValueError ("Nykyinen salasana on väärin")
+
+    if len(password) < 3:
+        raise ValueError ("Salasanan täytyy olla ainakin 3 merkkiä pitkiä")
+
+    if len(password) > 15:
+        raise ValueError ("Salasanan ei saa olla yli 15 merkkiä pitkä")
+
+    if password != confirm_password:
+        raise ValueError ("Salasanat eivät täsmää")
+
+    return True
