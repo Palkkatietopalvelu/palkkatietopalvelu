d12b5de04840ea46a87cfb1d73cf3b0fb42786fc
liisa <liisa-m@outlook.com>
2024-02-15 18:48:00 +0200

    update tests and fix pylint flaws

--- backend/src/tests/login_test.py
+++ backend/src/tests/login_test.py
@@ -1,66 +1,45 @@
 import unittest
-from db import db
 from app import app
-from models.user import User
-import controllers.users
-import controllers.login
-from flask import request, jsonify
-from werkzeug.security import generate_password_hash
-from models.user import User, db
+from initialize_db import initialize_database
 
 class TestLoginController(unittest.TestCase):
     def setUp(self):
-        with app.test_request_context():
-            username = "masa"
-            password = "masa123"
-            role = 1
-            new_user = controllers.users.create_user(testing="yes", username=username, 
-                        password=password, role=role)
+        initialize_database()
+        data = {"username": "masa", "password": "masa123", "role": 1}
+        app.test_client().post("/api/users", json=data)
 
     def test_login_with_correct_credentials(self):
         with app.test_request_context():
-            username = "masa"
-            password = "masa123"
-            login_attempt = controllers.login.login(testing="yes", username=username, password=password)
-            self.assertEqual(login_attempt[0].status, "200 OK")
-            self.assertEqual(login_attempt[1], 200)
+            data = {"username": "masa", "password": "masa123"}
+            response = app.test_client().post("/api/login", json=data)
+            self.assertEqual(response.status_code, 200)
 
     def test_login_with_incorrect_username_and_right_password(self):
         with app.test_request_context():
-            username = "bertta"
-            password = "masa123"
-            login_attempt = controllers.login.login(testing="yes", username=username, password=password)
-            self.assertEqual(login_attempt[0].status, "200 OK")
-            self.assertEqual(login_attempt[1], 401)
+            data = {"username": "bertta", "password": "masa123"}
+            response = app.test_client().post("/api/login", json=data)
+            self.assertEqual(response.status_code, 401)
 
     def test_login_with_incorrect_password_and_right_username(self):
         with app.test_request_context():
-            username = "masa"
-            password = "wrong"
-            login_attempt = controllers.login.login(testing="yes", username=username, password=password)
-            self.assertEqual(login_attempt[0].status, "200 OK")
-            self.assertEqual(login_attempt[1], 401)
+            data = {"username": "masa", "password": "wrong"}
+            response = app.test_client().post("/api/login", json=data)
+            self.assertEqual(response.status_code, 401)
 
     def test_login_with_incorrect_credentials(self):
         with app.test_request_context():
-            username = "hacker"
-            password = "password123"
-            login_attempt = controllers.login.login(testing="yes", username=username, password=password)
-            self.assertEqual(login_attempt[0].status, "200 OK")
-            self.assertEqual(login_attempt[1], 401)
+            data = {"username": "hacker", "password": "password123"}
+            response = app.test_client().post("/api/login", json=data)
+            self.assertEqual(response.status_code, 401)
 
     def test_login_with_no_username(self):
         with app.test_request_context():
-            username = ""
-            password = "password123"
-            login_attempt = controllers.login.login(testing="yes", username=username, password=password)
-            self.assertEqual(login_attempt[0].status, "200 OK")
-            self.assertEqual(login_attempt[1], 401)
+            data = {"username": "", "password": "password123"}
+            response = app.test_client().post("/api/login", json=data)
+            self.assertEqual(response.status_code, 401)
 
     def test_login_with_no_password(self):
         with app.test_request_context():
-            username = "hacker"
-            password = ""
-            login_attempt = controllers.login.login(testing="yes", username=username, password=password)
-            self.assertEqual(login_attempt[0].status, "200 OK")
-            self.assertEqual(login_attempt[1], 401)
+            data = {"username": "hacker", "password": ""}
+            response = app.test_client().post("/api/login", json=data)
+            self.assertEqual(response.status_code, 401)
