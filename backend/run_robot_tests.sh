595c37e42b85e4f7550f41dbc82b6fd4f662de04
Emma Lundström <1emma.lundstrom@gmail.com>
2024-02-19 17:37:17 +0200

    run robottests with bash

---
+++ backend/run_robot_tests.sh
@@ -0,0 +1,16 @@
+
+
+cd backend
+poetry run invoke start
+
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000/ping)" != "200" ]]; 
+  do sleep 1; 
+done
+
+poetry run invoke robottests
+
+status=$?
+
+kill $(lsof -t -i:5000)
+
+exit $status
