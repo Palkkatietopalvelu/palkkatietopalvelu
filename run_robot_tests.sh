b5bd2d5642de4e5a755393500a0e082763fa6fb5
Emma Lundström <1emma.lundstrom@gmail.com>
2024-02-19 18:20:36 +0200

    edit bash

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -3,7 +3,7 @@
 cd backend
 poetry run invoke start
 
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000/ping)" != "200" ]]; 
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000/)" != "200" ]]; 
   do sleep 1; 
 done
 
