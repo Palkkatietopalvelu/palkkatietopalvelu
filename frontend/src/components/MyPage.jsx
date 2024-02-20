2d398e0099a69f54a08ab3219e3db0bd995783a0
Aare <aare212@hotmail.com>
2024-02-20 08:58:07 +0200

    Client filter and green nav bar

--- frontend/src/components/MyPage.jsx
+++ frontend/src/components/MyPage.jsx
@@ -1,4 +1,8 @@
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { Table } from 'react-bootstrap'
+import { format } from 'date-fns'
+import { useState } from "react"
 import PasswordChange from './PasswordForm'
 import Notification from './Notification'
 import MyPageClient from './MyPageClient'
@@ -9,6 +13,13 @@ const MyPage = () => {
 
   if (!user) {
     return ('Et ole kirjautunut sisään')
+  }
+
+  const handleSearchword = (event) => {
+    event.target.value === ""
+      ? setFilteredCompanies(clients)
+      : setFilteredCompanies(clients.filter(
+        c => c.company.toLowerCase().includes(event.target.value.toLowerCase())))
   }
 
   return (
@@ -22,6 +33,37 @@ const MyPage = () => {
         ? <MyPageAdmin />
         : <MyPageClient />}
     </div>
+  )
+}
+
+const CompanyList = ({ filteredCompanies, handleSearchword }) => {
+  return (
+    <Table striped>
+      <thead>
+        <tr>
+          <th>Yritys</th>
+          <th>Eräpäivä</th>
+        </tr>
+        <tr>
+          <th><input onChange={handleSearchword} /></th>
+        </tr>
+      </thead>
+      <tbody>
+        {filteredCompanies
+          .map(client => {
+            return (
+              <tr key={client.id}>
+                <td>
+                  <Link to={`/client/${client.id}`}>
+                    {client.company}
+                  </Link>
+                </td>
+                <td>{format(client.deadlines[0], 'dd.MM.yyyy')}</td>
+              </tr>
+            )}
+          )}
+      </tbody>
+    </Table>
   )
 }
 
