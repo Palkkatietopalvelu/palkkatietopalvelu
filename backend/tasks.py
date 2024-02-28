e02d3e473b66d0c91ef038c11523e973164d2efc
liisa <liisa-m@outlook.com>
2024-02-28 11:16:43 +0200

    edit gitignore, tasks, readme

--- backend/tasks.py
+++ backend/tasks.py
@@ -15,9 +15,6 @@ def test(ctx):
 @task
 def coverage(ctx):
     ctx.run("coverage run --branch -m pytest src", pty=True)
-
-@task
-def coverage_report(ctx):
     ctx.run("coverage html", pty=True)
 
 @task
