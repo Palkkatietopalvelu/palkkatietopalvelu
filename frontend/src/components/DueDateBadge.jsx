e5cc0ef892cd87887d2083d2b2b03964adfe5235
Aare <aare212@hotmail.com>
2024-03-14 09:00:06 +0200

    Due date month filtering with digits

--- frontend/src/components/DueDateBadge.jsx
+++ frontend/src/components/DueDateBadge.jsx
@@ -11,7 +11,14 @@ const DueDateBadge = ({ client, now }) => {
   }
 
   let days_left = formatDistanceToNow(client.deadlines[0], now).split(' ')
-  if (days_left[0] === 'about')
+  //console.log("DueDateBadge days_left: ", days_left)
+  if (days_left[2] === "month") {
+    days_left = "noin kuukausi"
+  }
+  else if (days_left[1] === "months") {
+    days_left = "yli kuukausi"
+  }
+  else if (days_left[0] === 'about')
     days_left = '1 päivä'
   else
     days_left = days_left[0] + ' päivää'
