1495feccd37c32096a428bfebdd95317b43adfa7
liisa <liisa-m@outlook.com>
2024-03-21 18:18:42 +0200

    freeze client user if deactivated

--- backend/src/controllers/login.py
+++ backend/src/controllers/login.py
@@ -4,6 +4,7 @@ import jwt
 from werkzeug.security import check_password_hash
 from models.user import User
 from app import app
+from utilities.client_methods import get_status
 
 @app.route('/api/login', methods=['POST'])
 def login():
@@ -14,8 +15,10 @@ def login():
     user = User.query.filter_by(username=username).first()
 
     if user and check_password_hash(user.password, password):
+        if user.role == 2 and not get_status(user.username):
+            return jsonify({"error": "Tili jäädytetty deaktivoinnin takia."}), 401
         user_info = {"username": user.username, "id": user.id, "role": user.role}
         token = jwt.encode(user_info, os.environ.get('SECRET_KEY'), algorithm='HS256')
         return jsonify({"token": token, "username": user.username,
                         "id": user.id, "role": user.role}), 200
-    return jsonify({"error": "väärä käyttäjätunnus tai salasana"}), 401
+    return jsonify({"error": "Väärä käyttäjätunnus tai salasana"}), 401
