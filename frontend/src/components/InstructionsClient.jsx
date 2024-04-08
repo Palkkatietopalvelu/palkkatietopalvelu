b4986427fae7fc1a73553c521898b43a2029f7d2
Emma Lundström <1emma.lundstrom@gmail.com>
2024-04-08 10:46:16 +0300

    client instructions first draft

---
+++ frontend/src/components/InstructionsClient.jsx
@@ -0,0 +1,48 @@
+// ./intsructions (ohjeet, Client)
+import { useSelector } from 'react-redux'
+import React from 'react'
+
+const Header = (props) => {
+    return (
+      <div>
+        <h4>
+          {props.headline}
+          </h4>
+      </div>
+    )
+  }  
+
+const InstructionsClient = () => {
+  const user = useSelector(({ user }) => user)
+  const client = useSelector(({ clients }) => clients).find(c => c.email === user.username)
+  const parts = {part: ['Tiedostojen palautus', 'Tiedostojen poisto', 'Salasanan vaihto']}
+
+  if (!user) {
+    return ('Et ole kirjautunut sisään')
+  } else if (!client) {
+    return
+  }
+
+  return (
+    <div>
+      {user.role === 2 && <div> <hr />
+      <br /><Header headline={parts.part[0]} />
+      <br /><p>
+      Kotisivulla voi palauttaa aineistoja. Voit palauttaa joko valmiin tiedoston tai syöttää ohjelmassa olevan lomakkeen kautta jokaisen työntekijän tiedot. 
+      Voit myös ladata .csv tiedoston, johon voit täyttää tiedot ja sitten palauttaa sen tiedostona.
+      </p>
+      <br /><Header headline={parts.part[1]} />
+      <br /><p>
+      Kotisivulla voi myös poistaa tiedostoja. Painamalla poista nappia, tiedosto siirtyy roskakoriin. 
+      Roskakori poistaa yhden viikon vanhat tiedostot automaattisesti. Roskakorista voi myös manuaalisesti poistaa tiedoston kokonaan.
+      </p>
+      <br /><Header headline={parts.part[2]} />
+      <br /><p>
+      Omilla sivuilla voit vaihtaa salasanan, painamalla "Vaihda salasana."
+      </p>
+      </div>}
+    </div>
+  )
+}
+
+export default InstructionsClient
