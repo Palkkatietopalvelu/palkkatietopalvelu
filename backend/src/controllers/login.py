08e8ba8ea6ba167dd7927152ae75300bfe900bef
liisa <liisa-m@outlook.com>
2024-04-24 15:46:52 +0300

    refactor; email case insensitivity

--- backend/src/controllers/login.py
+++ backend/src/controllers/login.py
@@ -5,6 +5,7 @@ from werkzeug.security import check_password_hash
 from models.user import User
 from app import app
 from utilities.client_methods import get_status
+from sqlalchemy import func
 
 @app.route('/api/login', methods=['POST'])
 def login():
@@ -12,7 +13,7 @@ def login():
     username = data['username']
     password = data['password']
 
-    user = User.query.filter_by(username=username).first()
+    user = User.query.filter(func.lower(User.username) == func.lower(username)).first()
 
     if user and check_password_hash(user.password, password):
         if user.role == 2 and not get_status(user.username):
--- frontend/src/components/MyPage.jsx
+++ frontend/src/components/MyPage.jsx
@@ -38,7 +38,7 @@ const MyPage = () => {
               <div key={date}>
                 {new Date(date).toLocaleString('fi-FI',
                   { weekday: 'short', year: 'numeric', month: 'numeric', day: 'numeric' })}
-                {' '}{date == client.deadlines[0] && <DueDateBadge client={client} />}
+                {' '} <DueDateBadge deadline={client.deadlines[0]} />
               </div>)}</td></tr>
             <tr><td>Palkkakausi</td><td>{client.payperiod}</td></tr>
           </tbody>
